683392fd25cb58de25d65d6f602f466c0aa44c91
Melissa Kam <melissakam25@gmail.com>
2014-03-03 12:10:36 -0600

    Uploading initial project files

---
+++ RMSServer/src/com/gmail/utexas/rmsystem/Settings.java
@@ -0,0 +1,26 @@
+package com.gmail.utexas.rmsystem;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+
+@Entity
+public class Settings {
+	@Id public String id;
+	public int frequency, duration;
+	public String start, end;
+	public boolean schedule, roamingNotification, roamingAlarm, sleepwalkingNotification, sleepwalkingAlarm;
+	
+	public Settings(){
+		
+	}
+	
+	public Settings(String id){
+		this.id = id;
+	}	
+
+}
---
+++ RMSServer/src/com/gmail/utexas/rmsystem/SettingsServlet.java
@@ -0,0 +1,61 @@
+package com.gmail.utexas.rmsystem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Ref;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+public class SettingsServlet extends HttpServlet{
+
+    Logger log = Logger.getAnonymousLogger();
+	static {
+        ObjectifyService.register(Settings.class);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+    	log.info(req.getQueryString());    	
+    	String id = req.getParameter("id");
+    	log.info(id);
+    	
+        resp.setContentType("text/plain");
+        if(id != null){
+        	Settings settings = ofy().load().type(Settings.class).id(id).get();        
+        	Gson gson = new Gson();        
+        	resp.getWriter().println(gson.toJson(settings));
+        }        
+    }    
+    
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Gson gson = new Gson();
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } finally {
+            reader.close();
+        }
+        String content = sb.toString();
+        log.info(content);
+        
+        Settings settings = gson.fromJson(content, Settings.class);
+        ofy().save().entity(settings).now();
+
+        resp.setContentType("text/plain");;        
+    	resp.getWriter().println("Settings successfully saved.");
+    }
+}
